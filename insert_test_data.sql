baa77a71be97af97088004a8e3517796c155a1e1
Aymen Hamdaoui <yc.aymenhm@gmail.com>
2025-11-18 11:50:28 +0000

    adjusted hashedp

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,4 +3,4 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
-INSERT INTO users (username, first_name, last_name, email, hashedPassword) VALUES('gold', 'berties', 'books', 'bertiesbks@gold.ac.uk', 'smiths'), ('qwerty', 'q', 'w', 'qwerty@gold.ac.uk', 'qwerty'), ('trydeleteme', 'try', 'deleteme', 'trydm@gold.ac.uk', 'trydeleteme');
+INSERT INTO users (username, first_name, last_name, email, hashedPassword) VALUES('gold', 'berties', 'books', 'bertiesbks@gold.ac.uk', '$2b$10$fDpNAlVy3cXEkzYESfQUQeKUautzdfCjgRqg9fj1G8t4xb/TOiUEG'), ('qwerty', 'q', 'w', 'qwerty@gold.ac.uk', '$2b$10$eDC1RBR5kQgHv7aLpMI.WeA8aTY1sv4zaIa8u5p.8aBl0D6lNshnu');
